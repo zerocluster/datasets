03eb8609c4797fb91af8c81bf9074978d1f6f75b
zdm <zdm@softvisio.net>
2021-05-20 00:40:09 +0300

    maxmind module added

--- bin/install.js
+++ bin/install.js
@@ -1,11 +1,20 @@
 #!/usr/bin/env node
 
 import maxmind from "#lib/maxmind";
+import updater from "#lib/updater";
 
-const ok = await maxmind.update( { "force": true } );
+var ok = await maxmind.update( { "force": true } );
 
 if ( !ok ) {
     console.log( `Maxmind update error.` );
+
+    process.exit( 3 );
+}
+
+ok = await updater.update( { "force": true } );
+
+if ( !ok ) {
+    console.log( `Data update error.` );
 
     process.exit( 3 );
 }
