99c63ba401bbda0d88f535c67ae46765120beb67
zdm <zdm@softvisio.net>
2021-08-09 01:26:48 +0300

    datasets refactored

--- sources/build.js
+++ sources/build.js
@@ -4,28 +4,216 @@ import fs from "fs";
 import url from "url";
 import * as config from "#core/config";
 import area from "@turf/area";
+import bbox from "@turf/bbox";
+import csv from "fast-csv";
+import * as uule from "#core/api/google/uule";
 import CONST from "#lib/const";
 
-// const sources = url.fileURLToPath( new URL( "./", import.meta.url ) );
+const sources = url.fileURLToPath( new URL( "./", import.meta.url ) );
 const data = url.fileURLToPath( new URL( "../data", import.meta.url ) );
 
 const DATASETS = {
+    async ["datasets"] ( dataset, path ) {
+        const dbh = await sql.new( url.pathToFileURL( path ) );
+
+        await dbh.exec( sql`
+
+-- continent
+CREATE TABLE "continent" (
+    "id" text PRiMARY KEY NOT NULL, -- iso2
+    "iso2" text NOT NULL,
+    "name" text NOT NULL
+);
+
+CREATE UNIQUE INDEX idx_continent_name ON "continent" ("name");
+
+-- currency
+CREATE TABLE "currency" (
+    "id" text PRIMARY KEY NOT NULL, -- iso3
+    "iso3" text NOT NULL,
+    "name" text NOT NULL,
+    "symbol" text NOT NULL
+);
+
+CREATE UNIQUE INDEX idx_currency_name ON "currency" ("name");
+CREATE INDEX idx_currency_symbol ON "currency" ("symbol");
+
+-- country
+CREATE TABLE "country" (
+    "id" text PRIMARY KEY NOT NULL, -- iso2
+    "iso2" text NOT NULL,
+    "iso3" text NOT NULL,
+    "ison" text NOT NULL,
+    "name" text NOT NULL,
+    "official_name" text NOT NULL,
+    "flag" text NOT NULL,
+    "flag_unicode" text NOT NULL,
+    "continent" text NOT NULL,
+    "timezones" json,
+    "tld" text,
+    "postal_code_format" text,
+    "postal_code_regexp" text,
+    "calling_code" text,
+    "locales" json,
+    "languages" json NOT NULL,
+    "region" text NOT NULL,
+    "subregion" text NOT NULL,
+    "currencies" json NOT NULL,
+    "currency" text,
+    "coordinates" json
+);
+
+CREATE UNIQUE INDEX idx_country_iso3 ON "country" ("iso3");
+CREATE UNIQUE INDEX idx_country_name ON "country" ("name");
+
+-- language
+CREATE TABLE "language" (
+    "id" text PRIMARY KEY NOT NULL, -- iso3
+    "iso3" text NOT NULL,
+    "iso2" text,
+    "name" text NOT NULL,
+    "bibliographic" text
+);
+
+CREATE UNIQUE INDEX idx_language_iso2 ON "language" ("iso2");
+CREATE UNIQUE INDEX idx_language_name ON "language" ("name");
+
+-- timezone
+CREATE TABLE "timezone" (
+    "id" text PRIMARY KEY NOT NULL, -- name
+    "name" text NOT NULL,
+    "abbr" text NOT NULL
+);
+
+CREATE INDEX idx_timezone_abbr ON "timezone" ("abbr");
+
+    ` );
+
+        const _import = async function _import ( name, table ) {
+            const data = config.read( sources + "/" + name + ".json" );
+
+            await dbh.do( dbh.queryToString( sql`INSERT INTO "__name__"`.VALUES( data ) ).replace( "__name__", table ) );
+        };
+
+        await _import( "continents", "continent" );
+        await _import( "countries", "country" );
+        await _import( "currencies", "currency" );
+        await _import( "languages", "language" );
+        await _import( "timezones", "timezone" );
+
+        dbh.db.close();
+
+        return new Date();
+    },
+
+    // XXX find source file, or download from google
+    async ["google-geotargets"] ( dataset, path ) {
+        const dbh = await sql.new( url.pathToFileURL( path ) );
+
+        await dbh.exec( sql`
+CREATE TABLE IF NOT EXISTS "geotarget" (
+    "id" int4 PRIMARY KEY NOT NULL,
+    "name" text NOT NULL,
+    "canonical_name" text NOT NULL,
+    "parent_id" text,
+    "country" text,
+    "type" text,
+    "status" text,
+    "uule" text
+);
+
+CREATE INDEX idx_geotarget_canonical_name ON "geotarget" ("canonical_name");
+CREATE INDEX idx_geotarget_type ON "geotarget" ("type");
+CREATE INDEX idx_geotarget_status ON "geotarget" ("status");
+    ` );
+
+        const values = [];
+
+        await new Promise( resolve => {
+            csv.parseFile( data + "/geotargets-2021-07-27.csv", { "headers": headers => ["id", "name", "canonical_name", "parent_id", "country", "type", "status"] } )
+                .on( "error", error => console.error( error ) )
+                .on( "data", row => {
+                    row.type = row.type.toLowerCase();
+                    row.status = row.status.toLowerCase();
+
+                    row.uule = uule.encode( row.canonical_name );
+
+                    values.push( row );
+                } )
+                .on( "end", rowCount => {
+                    resolve();
+                } );
+        } );
+
+        await dbh.do( sql`INSERT INTO "geotarget"`.VALUES( values ) );
+
+        dbh.db.close();
+
+        return new Date();
+    },
+
+    async ["countries-geojson"] ( dataset, path ) {
+        const dbh = await sql.new( url.pathToFileURL( path ) );
+
+        await dbh.exec( sql`
+CREATE TABLE "countries" (
+    "country" text NOT NULL,
+    "minLongitude" float NOT NULL,
+    "minLatitude" float NOT NULL,
+    "maxLongitude" float NOT NULL,
+    "maxLatitude" float NOT NULL,
+    "geojson" json NOT NULL
+);
+
+CREATE INDEX "countries_country_bbox" ON "countries" ("country", "minLongitude", "minLatitude", "maxLongitude", "maxLatitude");
+    ` );
+
+        const json = config.read( data + "/ne_10m_admin_0_countries.geo.json" );
+
+        const values = [];
+
+        for ( const feature of json.features ) {
+            const polygons = feature.geometry.type === "MultiPolygon" ? feature.geometry.coordinates : [feature.geometry.coordinates];
+
+            for ( const coordinates of polygons ) {
+                const _bbox = bbox( { "type": "Polygon", coordinates } );
+
+                const record = {
+                    "country": feature.properties.ISO_A2,
+                    "minLongitude": _bbox[0],
+                    "minLatitude": _bbox[1],
+                    "maxLongitude": _bbox[2],
+                    "maxLatitude": _bbox[3],
+                    "geojson": { "type": "Polygon", coordinates },
+                };
+
+                values.push( record );
+            }
+        }
+
+        await dbh.do( sql`INSERT INTO "countries"`.VALUES( values ) );
+
+        dbh.db.close();
+
+        return new Date();
+    },
+
     async ["countries-triangles"] ( dataset, path ) {
         const dbh = await sql.new( url.pathToFileURL( path ) );
 
         await dbh.exec( sql`
-        CREATE TABLE "triangles" (
-            "country" text NOT NULL,
-            "max" float NOT NULL,
-            "point1_x" float NOT NULL,
-            "point1_y" float NOT NULL,
-            "point2_x" float NOT NULL,
-            "point2_y" float NOT NULL,
-            "point3_x" float NOT NULL,
-            "point3_y" float NOT NULL
-        );
+CREATE TABLE "triangles" (
+    "country" text NOT NULL,
+    "max" float NOT NULL,
+    "point1_x" float NOT NULL,
+    "point1_y" float NOT NULL,
+    "point2_x" float NOT NULL,
+    "point2_y" float NOT NULL,
+    "point3_x" float NOT NULL,
+    "point3_y" float NOT NULL
+);
 
-        CREATE INDEX "triangles_country_max" ON "triangles" ("country", "max");
+CREATE INDEX "triangles_country_max" ON "triangles" ("country", "max");
     ` );
 
         const json = config.read( data + "/ne_10m_admin_0_countries.geo.json" );
@@ -63,13 +251,13 @@ const DATASETS = {
                 }
             }
 
-            const data = [];
+            const values = [];
             let max = 0;
 
             for ( const triangle of index ) {
                 max += triangle.properties.area / totalArea;
 
-                data.push( {
+                values.push( {
                     country,
                     max,
                     "point1_x": triangle.coordinates[0][0][0],
@@ -81,8 +269,10 @@ const DATASETS = {
                 } );
             }
 
-            await dbh.do( sql`INSERT INTO "triangles"`.VALUES( data ) );
+            await dbh.do( sql`INSERT INTO "triangles"`.VALUES( values ) );
         }
+
+        dbh.db.close();
 
         return new Date();
     },
