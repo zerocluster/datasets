b9e3be93fce4aff0ec727ed9af882d3cbeee9334
zdm <zdm@softvisio.net>
2021-08-09 00:08:50 +0300

    datasets refactored

---
+++ sources/build-countries-triangles.sqlite.js
@@ -0,0 +1,86 @@
+import sql from "#core/sql";
+import earcut from "earcut";
+import fs from "fs";
+import url from "url";
+import * as config from "#core/config";
+import area from "@turf/area";
+import CONST from "#lib/const";
+
+const DATASET_NAME = "countries-triangles";
+
+const path = url.fileURLToPath( new URL( "../data/" + CONST.index[DATASET_NAME].name, import.meta.url ) );
+
+fs.rmSync( path, { "force": true } );
+
+const dbh = await sql.new( url.pathToFileURL( path ) );
+
+await dbh.exec( sql`
+        CREATE TABLE "triangles" (
+            "country" text NOT NULL,
+            "max" float NOT NULL,
+            "point1_x" float NOT NULL,
+            "point1_y" float NOT NULL,
+            "point2_x" float NOT NULL,
+            "point2_y" float NOT NULL,
+            "point3_x" float NOT NULL,
+            "point3_y" float NOT NULL
+        );
+
+        CREATE INDEX "triangles_country_max" ON "triangles" ("country", "max");
+    ` );
+
+const json = config.read( CONST.index["countries.geo.json"].location );
+
+for ( const feature of json.features ) {
+    const country = feature.properties.ISO_A3;
+
+    const index = [];
+
+    const polygons = feature.geometry.type === "MultiPolygon" ? feature.geometry.coordinates : [feature.geometry.coordinates];
+
+    let totalArea = 0;
+
+    for ( const polygon of polygons ) {
+        const vertexes = polygon[0];
+
+        const triangles = earcut( vertexes.flat() );
+
+        for ( let n = 0; n < triangles.length; n += 3 ) {
+            const v1 = vertexes[triangles[n]],
+                v2 = vertexes[triangles[n + 1]],
+                v3 = vertexes[triangles[n + 2]];
+
+            const triangle = {
+                "type": "Polygon",
+                "properties": {},
+                "coordinates": [[v1, v2, v3]],
+            };
+
+            triangle.properties.area = area( triangle );
+
+            totalArea += triangle.properties.area;
+
+            index.push( triangle );
+        }
+    }
+
+    const data = [];
+    let max = 0;
+
+    for ( const triangle of index ) {
+        max += triangle.properties.area / totalArea;
+
+        data.push( {
+            country,
+            max,
+            "point1_x": triangle.coordinates[0][0][0],
+            "point1_y": triangle.coordinates[0][0][1],
+            "point2_x": triangle.coordinates[0][1][0],
+            "point2_y": triangle.coordinates[0][1][1],
+            "point3_x": triangle.coordinates[0][2][0],
+            "point3_y": triangle.coordinates[0][2][1],
+        } );
+    }
+
+    await dbh.do( sql`INSERT INTO "triangles"`.VALUES( data ) );
+}
