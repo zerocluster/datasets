0e24548d899d5781ce2904d97a93e068c8ab6558
zdm <zdm@softvisio.net>
2021-08-13 09:39:04 +0300

    updater refactored

--- bin/update-datasets.js
+++ bin/update-datasets.js
@@ -1,8 +1,26 @@
 #!/usr/bin/env node
 
+import Cli from "#core/cli";
 import updater from "#lib/updater";
 
-const res = await updater.update( { "sync": true } );
+const CLI = {
+    "title": "Update datasets",
+    "options": {
+        "build": {
+            "description": "build datasets",
+            "default": false,
+            "schema": {
+                "type": "boolean",
+            },
+        },
+    },
+};
+
+await Cli.parse( CLI );
+
+if ( process.env.DATASETS_DOWNLOAD === "false" ) process.exit();
+
+const res = await updater.update( { "build": process.cli.options.build } );
 
 if ( !res.ok ) {
     console.log( `Datasets update error: ` + res );
