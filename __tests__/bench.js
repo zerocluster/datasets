fa69748a09e1f2312dbcf31afa7f6b5250dc9ada
zdm <zdm@softvisio.net>
2021-10-07 06:34:29 +0300

    feat: camel case api reverted

--- __tests__/bench.js
+++ __tests__/bench.js
@@ -2,6 +2,7 @@
 
 import Api from "#core/api";
 
+// const port = 10000;
 const port = 8080;
 
 const api = Api.new( `ws://devel:${port}/` );
@@ -10,29 +11,29 @@ const apiHttp = Api.new( `http://devel:${port}/` );
 const ip = "22.12.41.1";
 
 const t = {
-    async geoipAsn () {
+    async geoip_asn () {
         return api.call( "geoip/asn", ip );
     },
-    async geoipAsnHttp () {
+    async geoip_asn_http () {
         return apiHttp.call( "geoip/asn", ip );
     },
     async country () {
-        return api.call( "countries/get", "RU" );
+        return api.call( "country/get", "RU" );
     },
     async continent () {
-        return api.call( "continents/get", "AN" );
+        return api.call( "continent/get", "AN" );
     },
-    async geoipCountry () {
+    async geoip_country () {
         const ip = Math.ceil( Math.random() * 4_294_967_296 );
 
         return api.call( "geoip/country", ip );
     },
 };
 
-// console.log( await t.geoipAsn() );
-// console.log( await t.geoipAsnHttp() );
+// console.log( await t.geoip_asn() );
+// console.log( await t.geoip_asn_http() );
 // console.log( await t.country() );
 // console.log( await t.continent() );
-// console.log( await t.geoipCountry() );
+// console.log( await t.geoip_country() );
 
 bench( "datasets", t, 10000, 50 );
